a61b49308979cf5add979bc06bbd95fcd06f68ef
suvethar03 <suvethar03@gmail.com>
2023-03-16 12:26:20 +0530

    summa

---
+++ Suve/suv.java
@@ -0,0 +1,14 @@
+package suvepirai.com.tt;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
